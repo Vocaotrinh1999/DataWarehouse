8f02ef3a3b1a4b2a716f917daeffade742b095ae
Vocaotrinh1999 <55493337+Vocaotrinh1999@users.noreply.github.com>
2020-06-24 22:40:31 +0700

    commit

--- src/downloadfile/testDownload.java
+++ src/downloadfile/testDownload.java
@@ -4,12 +4,10 @@ public class testDownload {
 	public static void main(String[] args) {
 		Download download = new Download();
 		Account account = new Account("guest_access", "123456");
-		FileProperties fileProperties = new FileProperties("sinhvien*.*", "/volume1/ECEP/song.nguyen/DW_2020/data",
-<<<<<<< HEAD
-				"F:\\anh\\datawarehouse\\data");
-=======
+		FileProperties fileProperties = new FileProperties("sinhvien*.*", "/volume1/ECEP/song.nguyen/DW_2020/data","F:\\anh\\datawarehouse\\data");
+
 				"D:\\data");
->>>>>>> branch 'master' of https://github.com/Vocaotrinh1999/DataWarehouse
+
 		download.downloadFileFromNas(account, fileProperties);
 	}
 }
