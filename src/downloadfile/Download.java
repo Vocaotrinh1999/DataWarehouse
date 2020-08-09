eb45f24b6732d881087809f0366cfefb877e7102
phamdatDeveloper <phamdat.developer@gmail.com>
2020-08-09 23:32:29 +0700

    them ghi chu va chinh sua download

--- src/downloadfile/Download.java
+++ src/downloadfile/Download.java
@@ -2,7 +2,6 @@ package downloadfile;
 
 import java.io.File;
 import java.sql.ResultSet;
-<<<<<<< HEAD
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -13,16 +12,13 @@ import com.chilkatsoft.CkSsh;
 
 import GuiMail.SendMailTLS;
 import LoadDataStaging.ConnectDataConfig;
-import LoadDataStaging.InsertData2;
 
 public class Download {
 	private ConnectDataConfig connectDataConfig;
 	private SendMailTLS sendMail;
-	private InsertData2 insert;
 
 	public Download() {
 		connectDataConfig = new ConnectDataConfig();
-		insert = new InsertData2();
 	}
 
 	static {
@@ -60,35 +56,39 @@ public class Download {
 			}
 			downloadFileFromNas(id, fileName + "." + typeFile, host, port, userName, password, remoteDir, localDir);
 		} catch (Exception e) {
+			sendMail.sendMail("17130256@st.hcmuaf.edu.vn", "File Error", "Error sql");
 			e.printStackTrace();
 		}
 	}
 
 	public void downloadFileFromNas(int id, String fileFomatDownload, String host, Integer portName, String userName,
 			String password, String remoteDirName, String localDirName) {
-
 		String textSendMail = " ";
+		// unlock
 		CkGlobal glob = new CkGlobal();
 		glob.UnlockBundle("Waiting . . .");
 		CkSsh ssh = new CkSsh();
 
+		// Connect to an SSH server:
 		String hostname = host;
 		int port = portName;
 		boolean success = ssh.Connect(hostname, port);
 		if (success != true) {
-			System.out.println(ssh.lastErrorText());
+			sendMail.sendMail("17130256@st.hcmuaf.edu.vn", "File Error", "error connect");
 			return;
 		}
 		ssh.put_IdleTimeoutMs(5000);
 		success = ssh.AuthenticatePw(userName, password);
 		if (success != true) {
-			System.out.println(ssh.lastErrorText());
+			sendMail.sendMail("17130256@st.hcmuaf.edu.vn", "File Error", "Error connect");
 			return;
 		}
+
+		// Tạo scp để sử dụng đối tượng SSH
 		CkScp scp = new CkScp();
 		success = scp.UseSsh(ssh);
 		if (success != true) {
-			System.out.println(scp.lastErrorText());
+			sendMail.sendMail("17130256@st.hcmuaf.edu.vn", "File Error", "error connect");
 			return;
 		}
 		scp.put_SyncMustMatch(fileFomatDownload);
@@ -96,18 +96,18 @@ public class Download {
 		boolean bRecurse = false;
 		success = scp.SyncTreeDownload(remoteDirName, localDirName, mode, bRecurse);
 		if (success != true) {
-			System.out.println(scp.lastErrorText());
+			sendMail.sendMail("17130256@st.hcmuaf.edu.vn", "File Error", "Error download");
 			return;
 		}
-
+		// ngắt kết nối server
 		ssh.Disconnect();
+
 		// check trùng file và insert log
 		File file = new File(localDirName);
 		File[] listFile = file.listFiles();
 		int count = 0;
 		for (File file2 : listFile) {
 			if (file2.length() == 0) {
-				System.out.println("File error :" + file2.getName());
 				textSendMail = "File " + file2.getName() + " error";
 				sendMail.sendMail("17130256@st.hcmuaf.edu.vn", "File Error", textSendMail);
 			} else {
@@ -116,8 +116,6 @@ public class Download {
 				}
 			}
 		}
-
-		System.out.println("Download File Success And Insert " + count + " File To Log");
 		textSendMail = "Download File Success And Insert " + count + " File To Log";
 		sendMail.sendMail("17130024@st.hcmuaf.edu.vn", "File Error", textSendMail);
 	}
@@ -125,129 +123,6 @@ public class Download {
 	public void insertLog(String fileName, int id, String status) {
 		String sql = "insert into datawarehouse_configuration.`control.data_file`(file_name,data_config_id,file_status,dt_file_queued) value('"
 				+ fileName + "'," + id + ",'" + status + "','" + Timestamp.valueOf(LocalDateTime.now()) + "')";
-=======
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-
-import com.chilkatsoft.CkGlobal;
-import com.chilkatsoft.CkScp;
-import com.chilkatsoft.CkSsh;
-
-import GuiMail.SendMailTLS;
-import LoadDataStaging.ConnectDataConfig;
-import LoadDataStaging.InsertData2;
-
-public class Download {
-	private ConnectDataConfig connectDataConfig;
-	private SendMailTLS sendMail;
-	private InsertData2 insert;
-
-	public Download() {
-		connectDataConfig = new ConnectDataConfig();
-		insert = new InsertData2();
-	}
-
-	static {
-		try {
-			System.loadLibrary("chilkat");
-		} catch (UnsatisfiedLinkError e) {
-			System.err.println("Native code library failed to load.\n" + e);
-			System.exit(1);
-		}
-	}
-
-	// lay tu csdl len roi dowload
-	public void dowloadFile(int id) {
-		String fileName = "";
-		String typeFile = "";
-		String remoteDir = "";
-		String localDir = "";
-		String host = "";
-		Integer port = null;
-		String userName = "";
-		String password = "";
-		String sql = "SELECT * FROM datawarehouse_configuration.`control.data_file_configuration` where data_file_config_id ="
-				+ id;
-		try {
-			ResultSet r = connectDataConfig.selectDatabase(sql);
-			while (r.next()) {
-				fileName = r.getString("data_file_config_name");
-				typeFile = r.getString("data_file_type");
-				remoteDir = r.getString("remote_Dir");
-				localDir = r.getString("import_dir");
-				host = r.getString("hostname");
-				port = r.getInt("port");
-				userName = r.getString("username");
-				password = r.getString("password");
-			}
-			downloadFileFromNas(id, fileName + "." + typeFile, host, port, userName, password, remoteDir, localDir);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void downloadFileFromNas(int id, String fileFomatDownload, String host, Integer portName, String userName,
-			String password, String remoteDirName, String localDirName) {
-
-		String textSendMail = " ";
-		CkGlobal glob = new CkGlobal();
-		glob.UnlockBundle("Waiting . . .");
-		CkSsh ssh = new CkSsh();
-
-		String hostname = host;
-		int port = portName;
-		boolean success = ssh.Connect(hostname, port);
-		if (success != true) {
-			System.out.println(ssh.lastErrorText());
-			return;
-		}
-		ssh.put_IdleTimeoutMs(5000);
-		success = ssh.AuthenticatePw(userName, password);
-		if (success != true) {
-			System.out.println(ssh.lastErrorText());
-			return;
-		}
-		CkScp scp = new CkScp();
-		success = scp.UseSsh(ssh);
-		if (success != true) {
-			System.out.println(scp.lastErrorText());
-			return;
-		}
-		scp.put_SyncMustMatch(fileFomatDownload);
-		int mode = 3;
-		boolean bRecurse = false;
-		success = scp.SyncTreeDownload(remoteDirName, localDirName, mode, bRecurse);
-		if (success != true) {
-			System.out.println(scp.lastErrorText());
-			return;
-		}
-
-		ssh.Disconnect();
-		// check trùng file và insert log
-		File file = new File(localDirName);
-		File[] listFile = file.listFiles();
-		int count = 0;
-		for (File file2 : listFile) {
-			if (file2.length() == 0) {
-				System.out.println("File error :" + file2.getName());
-				textSendMail = "File " + file2.getName() + " error";
-				sendMail.sendMail("17130256@st.hcmuaf.edu.vn", "File Error", textSendMail);
-			} else {
-				if (checkDuplicate(file2.getName(), id) == false) {
-					insertLog(file2.getName(), id, "DL");
-				}
-			}
-		}
-
-		System.out.println("Download File Success And Insert " + count + " File To Log");
-		textSendMail = "Download File Success And Insert " + count + " File To Log";
-		sendMail.sendMail("17130024@st.hcmuaf.edu.vn", "File Error", textSendMail);
-	}
-
-	public void insertLog(String fileName, int id, String status) {
-		String sql = "insert into datawarehouse_configuration.`control.data_file`(file_name,data_config_id,file_status,dt_file_queued) value('"
-				+ fileName + "'," + id + ",'" + status + "','" + String.valueOf(LocalDateTime.now()) + "')";
->>>>>>> branch 'master' of https://github.com/Vocaotrinh1999/DataWarehouse
 		try {
 			connectDataConfig.perform(sql);
 		} catch (Exception e) {
